039ae5eb98a48fb48ea39c52503d41ff4d0f34f6
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-25 02:06:50 +0530

    Add axios call for registrations form

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Registration from './auth/Registration';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h1>Home</h1>
+        <Registration />
       </div>
     );
   }
---
+++ src/components/auth/Registration.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Registration extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      password_confirmation: '',
+      registrationErrors: ''
+    };
+  }
+
+  handleSubmit = e => {
+    const { email, password, password_confirmation } = this.state;
+    axios.post(
+      'https://localhost:3001/registrations',
+      {
+        user: {
+          email,
+          password,
+          password_confirmation
+        }
+      },
+      {
+        withCredentials: true
+      }
+    );
+    e.preventDefault();
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  render() {
+    const { email, password, password_confirmation } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            value={email}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type='password'
+            name='password'
+            id='password'
+            value={password}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type='password'
+            name='password_confirmation'
+            id='password_confirmation'
+            value={password_confirmation}
+            onChange={this.handleChange}
+            required
+          />
+
+          <button type='submit'>Register</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Registration;
