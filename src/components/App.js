103043b1a208620842b048436d42935a65169d2a
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-25 01:22:09 +0530

    Add basic routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
+import Home from "./Home";
+import Dashboard from "./Dashboard";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
