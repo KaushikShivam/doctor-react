9d2dcd51eb684b2d9b4443d7cdd94eeedf459ee2
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-27 02:40:18 +0530

    Finish book styles

--- src/components/DoctorBooking.js
+++ src/components/DoctorBooking.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import InfiniteCalendar from 'react-infinite-calendar';
+import TimePicker from 'rc-time-picker';
+import 'rc-time-picker/assets/index.css';
+import moment from 'moment';
+import like from '../assets/images/like.png';
+
 import 'react-infinite-calendar/styles.css';
 import leftWhite from '../assets/images/leftWhite.png';
 
@@ -8,11 +14,16 @@ class DoctorBooking extends Component {
     super(props);
     this.state = {
       today: new Date(),
-      selectedDate: null
+      selectedDate: null,
+      time: null
     };
   }
   handleDateSelect = date => {
     this.setState({ selectedDate: date });
+  };
+
+  handleTimeChange = time => {
+    this.setState({ time });
   };
 
   render() {
@@ -44,6 +55,34 @@ class DoctorBooking extends Component {
           disabledDays={[0, 6]}
           onSelect={this.handleDateSelect}
         />
+        <div className="time">
+          <p>Select a Time:</p>
+          <TimePicker
+            showSecond={false}
+            defaultValue={moment()
+              .hour(0)
+              .minute(0)}
+            className="time-picker"
+            onChange={this.handleTimeChange}
+            format={'h:mm a'}
+            use12Hours
+            inputReadOnly
+          />
+        </div>
+        <div className="detail">
+          <Link className="book-btn">Book Appointment</Link>
+          <h5 className="title">Dr John Doe</h5>
+          <p className="category">General Physician</p>
+          <p className="address">HSR Layout, Bangalore</p>
+          <div className="d-flex exp">
+            <p>$200</p>
+            <p>14 yrs of experience</p>
+            <p>
+              <img src={like} alt="likes" />
+              <span>126</span>
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
