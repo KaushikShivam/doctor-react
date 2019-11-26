e5dd07239c94465117a5e85802bddd8a7ea163f2
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-27 04:12:16 +0530

    Finish styling DoctorSearch page

--- src/components/DoctorSearch.js
+++ src/components/DoctorSearch.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
+import logo from '../assets/images/logo.png';
 
 class DoctorSearch extends Component {
   render() {
+    const { categories } = this.props;
     return (
       <div className="DoctorSearch">
         <div className="DoctorSearch-nav">
           <h2 className="title">Doctors</h2>
         </div>
-        <div className="search">
-          <input type="search" name="search" id="search" placeholder="Search for Doctors, specialists etc" />
+        <div className="container">
+          <div className="search">
+            <input
+              type="search"
+              name="search"
+              id="search"
+              placeholder="Search for Doctors, specialists etc"
+            />
+          </div>
+          <div>
+            <img className="logo" src={logo} alt="company logo" />
+            <h1>Search Doctors</h1>
+            <p>
+              Search by directly typing the doctors name, speciality, symptoms,
+              clinics, hospital, etc. You can also search by health concerns
+              listed below
+            </p>
+            <div className="container">
+              <div className="categories row">
+                {categories.map(category => (
+                  <div className="col-6 col-md-4 mb-4">
+                    <button className="category-btn">{category}</button>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
+
+DoctorSearch.defaultProps = {
+  categories: [
+    'General Doctor',
+    'Skin & Hair',
+    'Child Care',
+    'Women Health',
+    'Dentist',
+    'ENT',
+    'Homeopathy',
+    'Ayurveda',
+    'Heart'
+  ]
+};
 
 export default DoctorSearch;
