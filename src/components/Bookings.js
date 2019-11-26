9759090e97d19ee41989d7fcc6190586522b65d0
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-27 03:09:37 +0530

    Finish Bookings nav

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import BookingItem from './BookingItem';
 
+import leftWhite from '../assets/images/leftWhite.png';
+
 class Bookings extends Component {
   render() {
     return (
-      <div className="Bookings container">
-        <BookingItem />
-        <BookingItem />
-        <BookingItem />
-        <BookingItem />
-        <BookingItem />
+      <div className="Bookings">
+        <div className="Bookings-nav">
+          <img src={leftWhite} alt="Left arrow" />
+          <h2>Your Bookings</h2>
+        </div>
+        <div className="container">
+          <BookingItem />
+          <BookingItem />
+          <BookingItem />
+          <BookingItem />
+          <BookingItem />
+        </div>
       </div>
     );
   }
