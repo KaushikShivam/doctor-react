4e4b912a06435d40e44d27697fbb046d1eb78f11
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-26 23:08:19 +0530

    Style card

--- src/components/DoctorDetail.js
+++ src/components/DoctorDetail.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import leftWhite from '../assets/images/leftWhite.png';
 import email from '../assets/images/email.png';
+import like from '../assets/images/like.png';
 import callAnswer from '../assets/images/call-answer.png';
 
 class DoctorDetail extends Component {
@@ -8,28 +10,32 @@ class DoctorDetail extends Component {
     return (
       <div className="DoctorDetail">
         <div className="detail-card">
-          <div className="DoctorDetail-nav">
-            <img src={leftWhite} alt="left arrow" />
+          <div className="DoctorDetail-nav d-flex justify-content-between">
+            <Link to="/doctors">
+              <img src={leftWhite} alt="left arrow" />
+            </Link>
             <p className="location">Bangalore</p>
           </div>
           <div className="DoctorDetail-info">
-            <h4>Dr. John Doe</h4>
-            <div>
-              <img src={callAnswer} alt="Call Answer" />
+            <h4 className="title">Dr. John Doe</h4>
+            <div className="d-flex justify-content-center align-items-center">
+              <img className="cta" src={callAnswer} alt="Call Answer" />
               <img
+                className="avatar"
                 src="https://www.w3schools.com/w3images/avatar6.png"
                 alt="Avatar"
               />
-              <img src={email} alt="email" />
+              <img className="cta" src={email} alt="email" />
             </div>
-            <p>General Physician</p>
-            <div className="d-flex">
+            <p className="category">General Physician</p>
+            <div className="d-flex exp">
               <p>$200</p>
               <p>14 yrs of experience</p>
               <p>
                 <img src={like} alt="likes" />
                 <span>126</span>
               </p>
+            </div>
           </div>
         </div>
       </div>
