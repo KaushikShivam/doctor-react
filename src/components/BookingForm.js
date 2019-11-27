3079edcb57e52ef9ed72b5247b95dcb097596fc1
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-27 18:39:12 +0530

    Finish Doctor info

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import leftArrow from '../assets/images/left-arrow.png';
 
 class BookingForm extends Component {
@@ -8,6 +9,13 @@ class BookingForm extends Component {
         <div className="BookingForm-nav d-flex">
           <img className="left-icon" src={leftArrow} alt="Left icon" />
           <h2 className="title">Booking Detail</h2>
+        </div>
+        <div className="detail">
+          {/* <Link className="book-btn">Book Appointment</Link> */}
+          <h5 className="title">Dr John Doe</h5>
+          <p className="category">General Physician</p>
+          <p className="address">HSR Layout, Bangalore</p>
+          <p className="date">4th Dec 2019, 11:30 AM</p>
         </div>
       </div>
     );
