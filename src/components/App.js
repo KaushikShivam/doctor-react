e26abb030821538ca4a01506aead3a27a1272aab
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-29 04:11:35 +0530

    Check logged in status in the app js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import './App.css';
 
 import Landing from './pages/Landing';
 import Login from './auth/Login';
 import Register from './auth/Register';
 import Alert from '../components/Alert';
+import { loggedIn } from './../redux/actions/auth';
 
-const App = () => {
-  return (
-    <Fragment>
-      <Alert />
-      <Route exact path="/" component={Landing} />
-      <Switch>
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-    </Fragment>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.loggedIn();
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Alert />
+        <Route exact path="/" component={Landing} />
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Fragment>
+    );
+  }
+}
+
+App.propTypes = {
+  loggedIn: PropTypes.func.isRequired
 };
 
-export default App;
+export default connect(null, { loggedIn })(App);
