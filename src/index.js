718183de75855a5fcf0f3213d6a4772eecdb4e7e
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-29 01:10:58 +0530

    Refactor reduc files into a separate folder

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './styles/main.scss';
 
-import rootReducer from './reducers/index';
+import store from './redux/store';
 
 import App from './components/App';
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
 ReactDOM.render(
   <BrowserRouter>
